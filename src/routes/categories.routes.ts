02adf7ac4d7cc2f4ffed249cbf92ca87c0649c4d
Vinicius Meirelles <vnmeirelles@gmail.com>
2020-12-23 23:09:08 -0300

    Finalizaçao dos models, reposytory, services rotas da entidade Category

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,19 @@
+import { Request, Response, Router } from 'express';
+
+import CreateCategoryService from '../services/CreateCategoryService';
+
+const categoriesRouter = Router();
+
+categoriesRouter.post('/', async (request: Request, response: Response) => {
+  const { title } = request.body;
+
+  const createCategory = new CreateCategoryService();
+
+  const category = await createCategory.execute({
+    title,
+  });
+
+  return response.json(category);
+});
+
+export default categoriesRouter;
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,31 @@
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+}
+
+class CreateCategoryService {
+  public async execute({ title }: Request): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+
+    const checkCategoryExists = await categoryRepository.findOne({
+      where: { title },
+    });
+
+    if (checkCategoryExists) {
+      throw new AppError('Category already exists');
+    }
+
+    const category = categoryRepository.create({
+      title,
+    });
+
+    await categoryRepository.save(category);
+
+    return category;
+  }
+}
+
+export default CreateCategoryService;
